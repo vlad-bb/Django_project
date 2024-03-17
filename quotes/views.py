56f24c1bc2cfc262a5d5da080e673d6da67a262c
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2024-03-17 14:50:16 +0200

    upd

--- quotes/views.py
+++ quotes/views.py
@@ -1,15 +1,15 @@
 from django import db
 from django.shortcuts import render
 from django.core.paginator import Paginator
-
+from .models import Author, Tag, Quote
 # Create your views here.
 from .utils import get_mongodb
 
 
 
 def main(request, page=1):
-    db = get_mongodb()
-    quotes = db.quotes.find()
+    #db = get_mongodb()
+    quotes = Quote.objects.all()
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
